8d0c067e30b926104790dc7c21ff922cf3c2ce42
Vivian Leung <vivianleung19@gmail.com>
2014-03-02 01:08:11 +0000

    finished branchify with errors, will take out

--- branchify.js
+++ branchify.js
@@ -2,84 +2,55 @@
 
   // FIND / SET ORIGIN graph.node[0] (no parents)
   // do master first
-  var masterHeadNode = findMaster(0);
 
-   branchify(0, masterHeadNode);
 
-  var masterHeadNode = function(i) {
-    if (i >= heads.length) { 
-      console.log("error, no master branch. using first one as default.");
-      return getNode(heads[0].commit);
+  branchify(heads.master.sha, getNode(heads.master));
+  heads.forEach( function(bhead) {
+    if (bhead.name != "master") { branchify(bhead.sha, getNode(bhead)); }    
+  });
+
+  // FUNCTIONNNN bInx is iterating index val for heads 
+  var branchify = function(b, currN) {
+
+    var currP = currN.parents;
+
+    // base case: end of branch (first commit of repo)
+    if (currP.length == 0) { return; } //
     }
-    else if (heads[i].name == "master"){
-      heads[i] = [heads[0], heads[0] = heads[i]][0];
-      return getNode(heads[0].commit]);
-    }
-    else { return masterHeadNode(++i); }
-  }
 
-  var masterHead = heads.master;
+    // recursive cases:
+    else{
+      if (currP.length > 1){
 
-  // bInx is iterating index val for heads 
-  var branchify = function(b, n) {
+        var currPn = currP.map(function(d) {return getNode(d); };
+        var pull = null;
 
-    var numPar = n.parents.length;
-    // if reached end of branch (parent)
-    if (numPar == 0) { 
-      if (++b >= heads.length) { return; };
-      else { return branchify(b, getNode(heads[b])); }
-    }
-  
-    // either along a branch or at a branch event
-    // if at a merge node
-    else if (numPar > 1){
-      var pNodes = n.parents.map( function(d) { return getNode(d); });
+        // for each parent NODE
+        currPn.forEach(function(baseP) {
 
-      var pull;
-      // for each parent NODE
-      pNodes.forEach(function(h) {
-        if (pulldata[h.sha] != undefined) {
-          pull = pulldata[h.sha];
-          n.branch = pull.head;
-          h.branch = pull.head;
+          if (baseP.branch == undefined) { 
+            // find base
+            if (pulls[baseP.sha] != undefined) {
+              pull = pulls[baseP.sha];
+              baseP.branch = pull.base;
+              branchify(pull.base, baseP);
 
-          pNodes.forEach(function(base) {
-            if(pull.base.sha == e.sha){
-              e.branch = pull.base;
-
-              //  combine for each return call. 1. get node, 2. return with remembered branch and node name
-          var nextP = d.parents.map( function(d) {return getNode(d); });
-              e.parents.forEach( function(h) {nextP.push(getNode(h)); });
-
-              nextP.forEach( function(next) { 
-                return branchify(next.branch  ) 
+              // if found base, set head 
+              currPn.forEach(function(headP, a) {
+                if(pull.head.sha == headP.sha){ 
+                  headP.branch = pull.head;
+                  branchify(pull.head, headP);
+                }
               })
-
             }
+            else { branchify(baseP.branch, baseP); }
+          }
+        }); // end of currPn loop
 
-          })
-        }
-        
-      })
-
-
-
-
-
-      
-      });
+        // if pulls not found 
+        if (pulls == null) { 
+          currPn[0].branch = currN.branch;
+          branchify(currPn[0].branch, currPn[0]);
+        }      
+      }
     }
-
-    // if branch is already defined (e.g. part of master, or other)
-    // put inside not empty else if (n.branch != undefined) { 
-
-
-    // node is product of a merge
-    // first parent item is the host of the merge (GETS merged into)
-    // second (and subsequent parents) the ones that merge INTO the first
-    else {
-      n.branch = b; 
-      // FOR EACH PARENT
-      return branchify(b, getNode(n.parents[0])); 
-    }
-  }
